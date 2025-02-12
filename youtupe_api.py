5eb3be57b4b17f04a231f56da36a55e624a1b222
JamieAC1108 <jaaroncole2004@gmail.com>
2025-02-11 21:33:57 -0500

    commit

---
+++ youtupe_api.py
@@ -0,0 +1,35 @@
+import googleapiclient.discovery
+import googleapiclient.errors
+import pandas as pd
+
+api_service_name = "youtube"
+api_version = "v3"
+DEVELOPER_KEY = "AIzaSyC05pt4JqXZg11dA9TmswMGrepGdVTYmz4"
+
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+request = youtube.commentThreads().list(
+    part="snippet",
+    videoId="WNrB1Q9Rry0",
+    maxResults=100
+)
+response = request.execute()
+
+comments = []
+
+for item in response['items']:
+    comment = item['snippet']['topLevelComment']['snippet']
+    comments.append([
+        comment['authorDisplayName'],
+        comment['publishedAt'],
+        comment['updatedAt'],
+        comment['likeCount'],
+        comment['textDisplay']
+    ])
+
+df = pd.DataFrame(comments, columns=['author', 'published_at', 'updated_at', 'like_count', 'text'])
+
+df.head(10).to_csv('youtube_comments.txt', index=False, sep='\t')  # Using tab as the separator
+
+print("Output written to youtube_comments.txt")
